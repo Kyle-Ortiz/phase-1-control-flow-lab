1dd07a9fa17e3a03d11bfd358f3eaadd7f0d5326
Kyle Ortiz <kyleaortiz@gmail.com>
2021-06-29 10:58:16 -0500

    commit tree 7397c6099656ed4664a0024995043a464791124f

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num) {
+  if (num <= 400) {
+    return "This one is on me!";
+  } else if (num < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let answer = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return answer;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let response;
+
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+  }
+  return response;
 }
